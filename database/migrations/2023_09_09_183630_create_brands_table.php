74006ca0c26a1608519f66cf1a0bf7ed6d24838f
josegarcia <jose.garciarodriguez@evertecinc.com>
2023-09-09 14:10:42 -0500

    feat: implement brands migration

--- database/migrations/2023_09_09_183630_create_brands_table.php
+++ database/migrations/2023_09_09_183630_create_brands_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('url')->nullable();
+            $table->string('primary_hex')->nullable();
+            $table->boolean('is_visible')->default(false);
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('brands');
