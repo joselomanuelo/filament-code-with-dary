efda79771d44c37200444f691afd15a9a48c5e27
josegarcia <jose.garciarodriguez@evertecinc.com>
2023-09-09 13:55:36 -0500

    feat: implement categories migration

--- database/migrations/2023_09_09_183558_create_categories_table.php
+++ database/migrations/2023_09_09_183558_create_categories_table.php
@@ -6,20 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')
+                ->unique();
+            $table->foreignId('parent_id')
+                ->nullable()
+                ->constrained('categories')
+                ->cascadeOnDelete();
+            $table->boolean('is_visible')
+                ->default(false);
+            $table->longText('description')
+                ->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categories');
