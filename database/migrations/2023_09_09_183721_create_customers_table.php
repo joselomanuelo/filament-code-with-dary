404b3772a498a87aa1f7de076b62a76fdd2ac126
josegarcia <jose.garciarodriguez@evertecinc.com>
2023-09-09 15:27:50 -0500

    feat: implement customers migration

--- database/migrations/2023_09_09_183721_create_customers_table.php
+++ database/migrations/2023_09_09_183721_create_customers_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->date('date_of_birth');
+            $table->string('address');
+            $table->string('zip_code');
+            $table->string('city');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('customers');
