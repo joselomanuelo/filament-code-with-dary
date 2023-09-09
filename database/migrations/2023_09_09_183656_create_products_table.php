d9090c743ff433d0dba30762879c51c89685e919
josegarcia <jose.garciarodriguez@evertecinc.com>
2023-09-09 15:17:20 -0500

    feat: implement products migration

--- database/migrations/2023_09_09_183656_create_products_table.php
+++ database/migrations/2023_09_09_183656_create_products_table.php
@@ -6,20 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('brand_id')
+                ->constrained('brands')
+                ->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku')->unique();
+            $table->longText('description')->nullable();
+            $table->unsignedBigInteger('quantity');
+            $table->decimal('price', 10);
+            $table->boolean('is_visible')->default(false);
+            $table->boolean('is_featured')->default(false);
+            $table->enum('type', ['deliverable', 'downloadable'])
+                ->default('deliverable');
+            $table->date('published_at');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
